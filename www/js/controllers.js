05f9cc75f0b46dcb661e680494e536edefc0b1db
aaronksaunders <aaron@clearlyinnovative.com>
2016-05-18 18:31:38 -0400

    updated to the latest release of Firebase

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,11 +3,61 @@ angular.module('starter.controllers', [])
     BASE_URL: 'https://clearlyinnovative-firebasestarterapp.firebaseio.com/'
   })
 
-  .controller('DashCtrl', function ($scope) {
+  .controller('PhotosCtrl', function ($scope, $timeout, FirebaseDB) {
 
+    /**
+     * 
+     */
+    $scope.doAddPhoto = function () {
+
+      fetch('img/adam.jpg').then(function (_data) {
+        return _data.blob()
+      }).then(function (_blob) {
+        uploadTask = FirebaseDB.storage().ref('images/adam.jpg').put(_blob)
+
+        uploadTask.on('state_changed', function (snapshot) {
+          // Observe state change events such as progress, pause, and resume
+        }, function (error) {
+          // Handle unsuccessful uploads
+          return error
+        }, function () {
+          // Handle successful uploads on complete..
+          var downloadURL = uploadTask.snapshot.downloadURL;
+
+          var ref = FirebaseDB.database().ref('Trash-Talk/images');
+          ref.push({ imageURL: downloadURL, 'when': new Date() });
+
+          return downloadURL
+        });
+      })
+    }
+
+    /**
+     * 
+     */
+    function getFBPhotos(_selectedChat) {
+
+      var ref = FirebaseDB.database().ref('Trash-Talk/images');
+      ref.on("value", function (snapshot) {
+        console.log(snapshot.val());
+        var res = []
+        snapshot.forEach(function (_item) {
+          console.log(_item.val())
+          res.push(_item.val())
+        })
+        $timeout(function () {
+          $scope.images = res
+        }, 1);
+      }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+    }
+
+    // start here..
+    getFBPhotos();
   })
 
-  .controller('ChatsCtrl', function ($scope, $timeout, FIREBASE_CONFIG, Chats) {
+  .controller('ChatsCtrl', function ($scope, $timeout, FIREBASE_CONFIG, Chats, FirebaseDB) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -31,17 +81,16 @@ angular.module('starter.controllers', [])
      */
     $scope.addMessage = function (_data, _selectedChat) {
       var _selectedChat = _selectedChat || 'saturday-chat'
-      var baseRef = new Firebase(FIREBASE_CONFIG.BASE_URL);
-      var ref = baseRef.child('Trash-Talk/' + _selectedChat);
+      var ref = FirebaseDB.database().ref('Trash-Talk/' + _selectedChat);
       ref.push({ 'player': 'fred', 'message': _data, 'when': new Date() });
+
     }
 
     function getFBChats(_selectedChat) {
 
       var _selectedChat = _selectedChat || 'saturday-chat'
 
-      var baseRef = new Firebase(FIREBASE_CONFIG.BASE_URL);
-      var ref = baseRef.child('Trash-Talk/' + _selectedChat);
+      var ref = FirebaseDB.database().ref('Trash-Talk/' + _selectedChat);
       ref.on("value", function (snapshot) {
         console.log(snapshot.val());
         var res = []
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,76 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+  .factory('FirebaseDB', function () {
+    var instance = null;
+    var storageInstance = null
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    return {
+      initialize: function () {
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
+        // Initialize Firebase
+        var config = {
+          apiKey: "AIzaSyDVgjc8ph0ZfgUV3YfN2J0OvfOz2xAWZug",
+          authDomain: "newfirebaseapp-2ee6f.firebaseapp.com",
+          databaseURL: "https://newfirebaseapp-2ee6f.firebaseio.com",
+          storageBucket: "newfirebaseapp-2ee6f.appspot.com",
+        };
+        instance = firebase.initializeApp(config);
+        storageInstance = firebase.storage();
+      },
+      database: function () {
+        return instance.database()
+      },
+      storage: function () {
+        return storageInstance
       }
-      return null;
     }
-  };
-});
+  })
+
+  .factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var chats = [{
+      id: 0,
+      name: 'Ben Sparrow',
+      lastText: 'You on your way?',
+      face: 'img/ben.png'
+    }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'img/max.png'
+      }, {
+        id: 2,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'img/adam.jpg'
+      }, {
+        id: 3,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'img/perry.png'
+      }, {
+        id: 4,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'img/mike.png'
+      }];
+
+    return {
+      all: function () {
+        return chats;
+      },
+      remove: function (chat) {
+        chats.splice(chats.indexOf(chat), 1);
+      },
+      get: function (chatId) {
+        for (var i = 0; i < chats.length; i++) {
+          if (chats[i].id === parseInt(chatId)) {
+            return chats[i];
+          }
+        }
+        return null;
+      }
+    };
+  });
